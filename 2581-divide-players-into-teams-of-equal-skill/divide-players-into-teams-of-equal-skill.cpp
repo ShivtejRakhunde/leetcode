8a76ab8915e92cba118a709463d676abcba41585
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2024-10-04 18:56:04 +0530

    Time: 64 ms (58.20%) | Memory: 56.2 MB (92.60%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
+        //we will sort given vector first for easy pairing 
         sort(skill.begin(), skill.end());
         int n = skill.size();
-        long long totalChem = 0;
-        int cChem = 0;
-        int tChem = skill[0] + skill[n-1];
+        long long totalChem = 0; //stores total chemistry calculated by multiplying individual chemistries
+        int cChem = 0; //stores current chemistry of any team
+        int tChem = skill[0] + skill[n-1]; //sample team chemistry
+
+        //iterate through half of vector
         for(int i=0;i<n/2;i++){
-            cChem = skill[i]+skill[n-1-i];
-            if(cChem!=tChem){
+            cChem = skill[i]+skill[n-1-i]; //store current chem of team by adding ith from start and ith form end of vector
+            if(cChem!=tChem){ //if current and sample team chemistries are not equal exit
                 return -1;
             }
-            totalChem += (long long)skill[i]*(long long)skill[n-1-i];
+            totalChem += (long long)skill[i]*(long long)skill[n-1-i]; //calculate total chemistry by adding individual team chemistry
         }
-        return totalChem;
+        return totalChem; //return answer
     }
 };
