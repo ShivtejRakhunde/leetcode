7f032988392b570bf78349b1a482a916aed1ac51
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2024-10-07 20:27:19 +0530

    Time: 3 ms (89.38%) | Memory: 11.5 MB (61.24%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            if(st.empty()){
+                st.push(c);
+                continue;
+            }
+
+            if(c=='B'&& st.top()=='A'){
+                st.pop();
+            }
+            else if(c=='D' && st.top()=='C'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.size();
+    }
+};
