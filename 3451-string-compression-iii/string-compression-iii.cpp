252f2c1e3db63c5d1162646807e4ab45b3bccda1
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2024-11-04 15:54:25 +0530

    Time: 57 ms (23.93%) | Memory: 29.9 MB (24.06%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int pos = 0;
+
+        //continue till end of string
+        while(pos<word.length()){
+            int consecutiveCount = 0;
+
+            char currentChar = word[pos];
+
+            // Count consecutive occurrences (maximum 9)
+            while (pos < word.length() && consecutiveCount < 9 &&
+                   word[pos] == currentChar) {
+                consecutiveCount++;
+                pos++;
+            }
+
+            // Append count followed by character to result
+            comp += to_string(consecutiveCount) + currentChar;
+        }
+
+        return comp;
+    }
+};
