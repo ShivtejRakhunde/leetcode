89aa9a6323451be242ecd26f6538d014653ad5e9
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-01-01 17:07:44 +0530

    Time: 4 ms (11.39%) | Memory: 8.9 MB (18.99%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxScore(string s) {
+        int score = 0;
+        string left, right;
+
+        for(int i=0; i<s.length()-1; i++){
+            int curntScore = 0;
+            for(int j=0; j<=i; j++){
+                if(s[j]=='0'){
+                    curntScore++;
+                }
+            }
+            for(int j=i+1; j<=s.length(); j++){
+                if(s[j]=='1'){
+                    curntScore++;
+                }
+            }
+            score = max(score, curntScore);
+        }
+        return score;
+    }
+};
