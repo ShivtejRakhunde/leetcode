c57818ad4af34870d3f062bae93a5fa3666d9fc1
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-02-02 17:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (22.96%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        if(n<=1){
+            return true;
+        }
+
+        int inversionK = 0;
+
+        // For every pair, count the number of inversions.
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                ++inversionK;
+            }
+        }
+
+        // Also check between the last and the first element due to rotation
+        if (nums[0] < nums[n - 1]) {
+            ++inversionK;
+        }
+
+        return inversionK <= 1;
+    }
+};
