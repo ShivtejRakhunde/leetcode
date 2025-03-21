e1f8425a139a62e1a592e64f4a31f4dcc4504dc9
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-03-22 01:16:57 +0530

    Time: 11 ms (34.50%) | Memory: 27.5 MB (55.22%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int> countB;
+        vector<int> countB; //holds '1's in each row
 
         for(auto& t: bank){
-            int k = count(t.begin(), t.end(), '1');
+            int k = count(t.begin(), t.end(), '1'); //count of '1' in each string
             if(k>0){
-                countB.push_back(k);
+                countB.push_back(k);    //exclude string having only '0's
             }
         }
 
         int ans = 0, n = countB.size();
 
+        //should contain atleast 2 rows with security beams
         if(n<2){
             return 0;
         }
