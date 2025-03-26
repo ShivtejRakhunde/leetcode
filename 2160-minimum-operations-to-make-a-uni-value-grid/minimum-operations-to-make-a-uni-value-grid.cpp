062790175c6e248536e4632ca71e6a65bdadc1e9
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-03-26 19:48:00 +0530

    Time: 51 ms (31.31%) | Memory: 90.1 MB (48.02%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int CheckOperations(vector<int>& temp, int x){
+        int op = 0;
+        int CommNum = temp[temp.size()/2];
+        for(int i=0; i<temp.size(); i++){
+            if(temp[i]%x!=CommNum%x){
+                return -1;
+            }
+            else{
+                op += abs(CommNum - temp[i])/x;
+            }
+        }
+        return op;
+    }
+
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> temp;
+
+        for (const auto& row : grid) {
+            for (int num : row) {
+                temp.push_back(num);
+            }
+        }
+
+        sort(temp.begin(), temp.end());
+
+        return CheckOperations(temp, x);
+    }
+};
