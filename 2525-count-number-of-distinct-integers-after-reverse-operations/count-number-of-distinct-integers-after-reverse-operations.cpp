34ff3e85af001946558746fe730cb9e191af82d4
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-04-05 23:42:03 +0530

    Time: 175 ms (78.21%) | Memory: 110.5 MB (80.08%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -4,11 +4,10 @@ public:
         unordered_set<int> st(nums.begin(), nums.end());
         int n = nums.size();
         for(int i=0; i<n; i++){
-            int digit = 0, sum = 0; 
+            int sum = 0; 
             int temp = nums[i];
             while(temp>0){
-                digit = temp%10;
-                sum = sum*10 + digit;
+                sum = sum*10 + temp%10;
                 temp /= 10;
             }
             st.insert(sum);
